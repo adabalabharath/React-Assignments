931119f0ad324a0091ad98a3794df65d80f0e38f
adabalabharath <saibharath.adabala@gmail.com>
2024-04-18 18:12:21 +0530

    debounced

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,53 +1,63 @@
-    import React, { useState } from 'react'
-    import Suggestions from '../Comps/Suggestions'
-    import './Home.css'
-    const Home = ({click,set}) => {
+import React, { useEffect, useState } from 'react';
+import Suggestions from '../Comps/Suggestions';
+import './Home.css';
+import _ from 'lodash'; // Import Lodash
 
-        const[text,setText]=useState("");
-        const[fil,setFil]=useState([])
-        const handleChange=(e)=>{
-            let searchText=e.target.value;
-            setText(searchText);
-            set(searchText);
-            debounce(searchText);
-        }
-    const get=async(text)=>{
-        const response = await fetch('users.json');
-                const users = await response.json();
-                const filteredUsers = users.filter(user =>
-                text && Object.values(user).some(value =>
-                        value.toLowerCase().includes(text.toLowerCase())
-                    )
-                );
-                setFil(filteredUsers);
-        
-    }
-    
-        const debounce=(text)=>{
-        let timer = setTimeout(() => {
-        get(text)
-    }, 1000);
+const Home = ({ click, set }) => {
+  const [fil, setFil] = useState([]);
+  const [input, setInput] = useState("");
 
-    return () => clearTimeout(timer)
-    }
+  const handleChange = (e) => {
+    let searchText = e.target.value;
+    setInput(searchText);
+    set(searchText);
+    // Call the debounced function
+    //_.debounce(get, 1000)(searchText); // Pass searchText as an argument
+  };
 
-    return (
-        <div className='container'>
-            <input type='text' className='input' placeholder='Enter something'value={text}  onChange={handleChange} style={{width:'300px'}}/>
-            <div className='result'>
-                <ul>
-            {fil.map((e,index)=>{
-                return(
-                    < div className='result' key={index}>
-                <li className='suggestion'>
-                    <Suggestions  names={e.name} data={e} clickfunc={click} />
-                </li> 
-                </div>  
-                )
-            })}</ul>
+  useEffect(() => {
+  const debouncedGet = _.debounce(get, 1000);
+  debouncedGet(input);
+
+  // Cleanup function to prevent memory leaks
+  return () => debouncedGet.cancel();
+}, [input]);
+
+  const get = async (text) => {
+    const response = await fetch('users.json');
+    const users = await response.json();
+    const filteredUsers = users.filter(user =>
+      text && Object.values(user).some(value =>
+        value.toLowerCase().includes(text.toLowerCase())
+      )
+    );
+    setFil(filteredUsers);
+  };
+
+  return (
+    <div className='container'>
+      <input
+        type='text'
+        className='input'
+        placeholder='Enter something'
+        onBlur={() => _.debounce(get, 1000)(input)}
+        onChange={handleChange}
+        style={{ width: '300px' }}
+        value={input}
+      />
+      <div className='result'>
+        <ul>
+          {fil.map((e, index) => (
+            <div className='result' key={index}>
+              <li className='suggestion'>
+                <Suggestions names={e.name} data={e} clickfunc={click} />
+              </li>
             </div>
-        </div>
-    )
-    }
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-    export default Home
+export default Home;
